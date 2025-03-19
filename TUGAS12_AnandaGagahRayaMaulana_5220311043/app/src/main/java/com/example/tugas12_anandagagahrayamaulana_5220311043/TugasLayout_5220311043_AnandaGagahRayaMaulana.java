4906819fabf83b5268bdbe8bdfc985a5842ba946
Rayamaulana12 <rayamaulana1303@gmail.com>
2025-03-19 16:01:28 +0700

    Initial commit

---
+++ TUGAS12_AnandaGagahRayaMaulana_5220311043/app/src/main/java/com/example/tugas12_anandagagahrayamaulana_5220311043/TugasLayout_5220311043_AnandaGagahRayaMaulana.java
@@ -0,0 +1,24 @@
+package com.example.tugas12_anandagagahrayamaulana_5220311043;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TugasLayout_5220311043_AnandaGagahRayaMaulana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tugas_layout5220311043_ananda_gagah_raya_maulana);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
